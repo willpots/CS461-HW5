0ae082251c327f17f63c95109e8cbc7059e7c8ee
Will Potter <will@wpotter.me>
2012-04-11 13:53:08 -0400

    added working smooth shaded VP

--- pipeline/vertex/SmoothShadedVP.java
+++ pipeline/vertex/SmoothShadedVP.java
@@ -1,9 +1,11 @@
 package pipeline.vertex;
 
+import java.awt.Point;
 import java.util.Vector;
 
 import javax.vecmath.Color3f;
 import javax.vecmath.Point3f;
+import javax.vecmath.Point4f;
 import javax.vecmath.Vector2f;
 import javax.vecmath.Vector3f;
 import javax.vecmath.Vector4f;
@@ -25,13 +27,12 @@ public class SmoothShadedVP extends VertexProcessor {
     protected Matrix4f m = new Matrix4f();
     
     /** This is the projectionMatrix matrix. */
-    protected Matrix4f p = new Matrix4f();
+    protected Matrix4f projection = new Matrix4f();
     
     /** This is the viewportMatrix matrix. */
-    protected Matrix4f v = new Matrix4f();
+    protected Matrix4f viewport = new Matrix4f();
     
-    public static Vector<PointLight> lights;
- 
+    protected Vector<PointLight> lights = new Vector<PointLight>();
     // The number of fragment attributes provided to the fragment processor
     public int nAttr() {
         return 3;   // surface color (r,g,b)
@@ -43,7 +44,8 @@ public class SmoothShadedVP extends VertexProcessor {
     public void updateTransforms(Pipeline pipe) {
         // TODO 1
         m.set(pipe.modelviewMatrix);
-        lights = Pipeline.lights;
+        projection.set(pipe.projectionMatrix);
+        viewport.set(pipe.viewportMatrix);
     }
    
     /**
@@ -53,40 +55,61 @@ public class SmoothShadedVP extends VertexProcessor {
         // TODO 1
     	Vector4f normal = new Vector4f(n.x,n.y,n.z,0);
         output.v.set(v.x, v.y, v.z, 1);
-    	
         // multiply v by modelview matrix, this gives vertex pos in "eye space"
         m.rightMultiply(output.v);
-
     	// do the same with the normal
         m.rightMultiply(normal);
         
         // compute color at vertex:
-
+        Color3f ct = new Color3f(c);
         // start with c scaled by Pipeline.ambientIntensity
-        c.scale(Pipeline.ambientIntensity);
-        
-        // for each light, add diffuse and specular term, using normal, light direction, and eye direction
-        // note that light positions are given in eye space, and the camera is at the origin
-        for(int i=0;i<lights.size();i++) {
-        	Vector3f lv = new Vector3f();
-        	PointLight l = lights.get(i);
-        	lv.sub(l.getPosition(), v);
-        	l.getIntensity();
-        	l.getPosition();
+        ct.scale(Pipeline.ambientIntensity);
+
+        for(int i=0;i<Pipeline.lights.size();i++) {
+        	PointLight l = Pipeline.lights.get(i);
+        	Vector3f p = new Vector3f(output.v.x,output.v.y,output.v.z);
+        	Vector3f lV = new Vector3f();
+        	Vector3f h = new Vector3f();
+        	Vector3f norm = new Vector3f(normal.x,normal.y,normal.z);
+        	Vector3f toEye = new Vector3f(p);
+        	toEye.scale(-1);
+	        // for each light, add diffuse and specular term, using normal, light direction, and eye direction
+	        // note that light positions are given in eye space, and the camera is at the origin
+	
+	        // diffuse term uses vertex color c and light color
+	        
+	        // specular term uses Pipeline.specularColor and assumes light color is full white
+	        // also uses Pipeline.specularExponent
+			// Calculate l and h
+        	
+			lV.sub(l.getPosition(),p);
+			lV.normalize();
+			h.add(toEye,lV);
+			h.normalize();
+			Color3f intensity  = l.getIntensity();
+			double a = Math.max(0,lV.dot(norm));
+			double b = Math.pow(Math.max(0, norm.dot(h)),Pipeline.specularExponent);
+			// Lambertian Component
+			ct.x += (c.x * intensity.x * a);
+			ct.y += (c.y * intensity.y * a);
+			ct.z += (c.z * intensity.z * a);
+			// Phong Component
+			ct.x += (Pipeline.specularColor.x * intensity.x * b);
+			ct.y += (Pipeline.specularColor.y * intensity.y * b);
+			ct.z += (Pipeline.specularColor.z * intensity.z * b);
+			
         }
-        // diffuse term uses vertex color c and light color
-        
-        // specular term uses Pipeline.specularColor and assumes light color is full white
-        // also uses Pipeline.specularExponent
-        
+    	ct.clamp(0,1);
         // in the end, clamp the resulting color to [0, 1] and store in attributes
         output.setAttrs(nAttr());
-        output.attrs[0] = 1; // red for now
-        output.attrs[1] = 0;
-        output.attrs[2] = 0;
-
+        output.attrs[0] = ct.x; // red for now
+        output.attrs[1] = ct.y;
+        output.attrs[2] = ct.z;
         
         // also project vertex position to screen space
+        projection.rightMultiply(output.v);
+        viewport.rightMultiply(output.v);
+        
     }
 
 }
