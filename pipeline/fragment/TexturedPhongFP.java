03b264f6512f77d25f0d70d178a3d2a991e696f7
Daniel Trauner <danielhtrauner@gmail.com>
2012-04-19 16:27:38 -0400

    Added broken TexturedPhong files

--- pipeline/fragment/TexturedPhongFP.java
+++ pipeline/fragment/TexturedPhongFP.java
@@ -5,7 +5,10 @@ import javax.vecmath.Point3f;
 import javax.vecmath.Vector2f;
 import javax.vecmath.Vector3f;
 
+import pipeline.Pipeline;
+import pipeline.PointLight;
 import pipeline.misc.Fragment;
+import pipeline.misc.FrameBuffer;
 
 /**
  * This FP works just like the PhongShadedFP, but also incorporates a texture.
@@ -13,11 +16,66 @@ import pipeline.misc.Fragment;
  * @author ags
  */
 public class TexturedPhongFP extends PhongShadedFP {
+	// The number of fragment attributes expected from the vertex processor
+	public int nAttr() {
+		System.out.println("Testing2");
 
-    // The number of fragment attributes expected from the vertex processor
-    public int nAttr() {
-        return 8;   // normal (x,y,z), texture coordinates (u,v), fragment position (x,y,z)
-    }
+		return 8; 	// normal (x,y,z), texture coordinates (u,v), fragment
+					// position (x,y,z)
 
-    // TODO 2
+	}
+
+	/**
+	 * @see FragmentProcessor#fragment(Fragment, FrameBuffer)
+	 */
+	
+	
+	public void fragment(Fragment f, FrameBuffer fb) {
+		// TODO 2
+		System.out.println("Testing");
+
+		Color3f textureColor = new Color3f();
+		Vector2f inCoords = new Vector2f(f.attrs[4], f.attrs[5]);
+		texture.sample(inCoords, textureColor);
+
+		Color3f outColor = new Color3f(0,0,0);
+		Vector3f lV = new Vector3f();
+		Vector3f h = new Vector3f();
+
+		textureColor.scale(Pipeline.ambientIntensity);
+
+		Vector3f norm = new Vector3f(f.attrs[1], f.attrs[2], f.attrs[3]);
+		norm.normalize();
+
+		Point3f position = new Point3f(f.attrs[6], f.attrs[7], f.attrs[8]);
+		Vector3f toEye = new Vector3f(position);
+		toEye.scale(-1);
+		toEye.normalize();
+
+		for (int i = 0; i < Pipeline.lights.size(); i++) {
+			PointLight l = Pipeline.lights.get(i);
+			lV.sub(l.getPosition(), position);
+			lV.normalize();
+			h.add(toEye, lV);
+			h.normalize();
+
+			// also uses Pipeline.specularExponent
+			double b = Math.pow(Math.max(0, norm.dot(h)),
+					Pipeline.specularExponent);
+
+			// Phong Component
+			outColor.x += (Pipeline.specularColor.x * b);
+			outColor.y += (Pipeline.specularColor.y * b);
+			outColor.z += (Pipeline.specularColor.z * b);
+
+		}
+
+		outColor.clamp(0, 1);
+
+		System.out.println(outColor);
+		
+		if (f.attrs[0] < fb.getZ(f.x, f.y))
+			fb.set(f.x, f.y, outColor.x*textureColor.x, outColor.y*textureColor.y, outColor.z*textureColor.z, f.attrs[0]);
+	}
+
 }
--- pipeline/vertex/TexturedFragmentShadedVP.java
+++ pipeline/vertex/TexturedFragmentShadedVP.java
@@ -1,9 +1,14 @@
 package pipeline.vertex;
 
+import java.util.Vector;
+
 import javax.vecmath.Color3f;
 import javax.vecmath.Vector2f;
 import javax.vecmath.Vector3f;
+import javax.vecmath.Vector4f;
 
+import pipeline.PointLight;
+import pipeline.math.Matrix4f;
 import pipeline.misc.Vertex;
 
 /**
@@ -15,20 +20,58 @@ import pipeline.misc.Vertex;
  */
 public class TexturedFragmentShadedVP extends FragmentShadedVP {
 
-    // The number of fragment attributes provided to the fragment processor
-    public int nAttr() {
-        return 8;   // normal (x,y,z), texture coordinates (u,v), fragment position (x,y,z)
-    }
+	/** This is the modelView matrix. */
+	protected Matrix4f m = new Matrix4f();
 
+	/** This is the projectionMatrix matrix. */
+	protected Matrix4f projection = new Matrix4f();
 
-    /**
-     * @see VertexProcessor#vertex(Vector3f, Color3f, Vector3f, Vector2f, Vertex)
-     */
-    public void vertex(Vector3f v, Color3f c, Vector3f n, Vector2f t, Vertex output) {
-        // TODO 2
-        
-        output.setAttrs(nAttr()); // placeholder
+	/** This is the viewportMatrix matrix. */
+	protected Matrix4f viewport = new Matrix4f();
 
-    }
+	protected Vector<PointLight> lights = new Vector<PointLight>();
+
+	// The number of fragment attributes provided to the fragment processor
+	public int nAttr() {
+		return 8; // normal (x,y,z), texture coordinates (u,v), fragment
+					// position (x,y,z)
+	}
+
+	/**
+	 * @see VertexProcessor#vertex(Vector3f, Color3f, Vector3f, Vector2f,
+	 *      Vertex)
+	 */
+	public void vertex(Vector3f v, Color3f c, Vector3f n, Vector2f t,
+			Vertex output) {
+		
+		// TODO 2
+		Vector4f normal = new Vector4f(n.x, n.y, n.z, 0);
+		output.v.set(v.x, v.y, v.z, 1);
+		// multiply v by modelview matrix, this gives vertex pos in "eye space"
+		m.rightMultiply(output.v);
+		// do the same with the normal
+		m.rightMultiply(normal);
+
+		// compute color at vertex:
+		Color3f ct = new Color3f(c);
+		// start with c scaled by Pipeline.ambientIntensity
+
+		output.setAttrs(nAttr());
+		output.attrs[0] = normal.x;
+		output.attrs[1] = normal.y;
+		output.attrs[2] = normal.z;
+
+		output.attrs[3] = t.x;
+		output.attrs[4] = t.y;
+
+		output.attrs[5] = output.v.x;
+		output.attrs[6] = output.v.y;
+		output.attrs[7] = output.v.z;
+
+		// also project vertex position to screen space
+		projection.rightMultiply(output.v);
+		viewport.rightMultiply(output.v);
+
+	}
 
 }
