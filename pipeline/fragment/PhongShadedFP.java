2b65510fbab8ecf052353ddd461a239ce5038e3d
Will Potter <will@wpotter.me>
2012-04-19 00:54:37 -0400

    added work as of 1 on Thursday morning

--- pipeline/fragment/PhongShadedFP.java
+++ pipeline/fragment/PhongShadedFP.java
@@ -2,6 +2,7 @@ package pipeline.fragment;
 
 import javax.vecmath.Color3f;
 import javax.vecmath.Point3f;
+import javax.vecmath.Vector2f;
 import javax.vecmath.Vector3f;
 
 import pipeline.Pipeline;
@@ -26,6 +27,49 @@ public class PhongShadedFP extends FragmentProcessor {
      * @see FragmentProcessor#fragment(Fragment, FrameBuffer)
      */
     public void fragment(Fragment f, FrameBuffer fb) {
-        // TODO 2
-    }
+		Color3f inColor = new Color3f(f.attrs[0],f.attrs[1],f.attrs[2]);
+		Color3f outColor = new Color3f(0,0,0);
+		Vector3f lV = new Vector3f();
+		Vector3f h = new Vector3f();
+
+		inColor.scale(Pipeline.ambientIntensity);
+		Vector3f norm = new Vector3f(f.attrs[3],f.attrs[4],f.attrs[5]);
+		norm.normalize();
+
+		Point3f position = new Point3f(f.attrs[6],f.attrs[7],f.attrs[8]);
+		Vector3f toEye = new Vector3f(position);
+		toEye.scale(-1);
+
+		for (int i = 0; i < Pipeline.lights.size(); i++) {
+			PointLight l = Pipeline.lights.get(i);
+			lV.sub(l.getPosition(), position);
+			lV.normalize();
+			h.add(toEye, lV);
+			h.normalize();
+
+			Color3f intensity = l.getIntensity();
+			// Perform Math Calculations before rather than trying to iterate
+			// through the Math library 8129418239 times.
+			double a = Math.max(0, lV.dot(norm));
+			// also uses Pipeline.specularExponent
+			double b = Math.pow(Math.max(0, norm.dot(h)), Pipeline.specularExponent);
+
+
+			// Lambertian Component
+			
+			// diffuse term uses vertex color c and light color
+			outColor.x += (inColor.x * intensity.x * a);
+			outColor.y += (inColor.y * intensity.y * a);
+			outColor.z += (inColor.z * intensity.z * a);
+			// Phong Component
+			outColor.x += (Pipeline.specularColor.x * intensity.x * b);
+			outColor.y += (Pipeline.specularColor.y * intensity.y * b);
+			outColor.z += (Pipeline.specularColor.z * intensity.z * b);
+
+		}
+		outColor.clamp(0, 1);
+		if (f.attrs[2] < fb.getZ(f.x, f.y)) {
+			fb.set(f.x, f.y, outColor.x, outColor.y, outColor.z, f.attrs[2]);
+		}
+	}
 }
--- pipeline/vertex/FragmentShadedVP.java
+++ pipeline/vertex/FragmentShadedVP.java
@@ -55,8 +55,7 @@ public class FragmentShadedVP extends VertexProcessor {
 	 * @see VertexProcessor#vertex(Vector3f, Color3f, Vector3f, Vector2f,
 	 *      Vertex)
 	 */
-	public void vertex(Vector3f v, Color3f c, Vector3f n, Vector2f t,
-			Vertex output) {
+	public void vertex(Vector3f v, Color3f c, Vector3f n, Vector2f t, Vertex output) {
 		// TODO 2
 		Vector4f normal = new Vector4f(n.x, n.y, n.z, 0);
 		output.v.set(v.x, v.y, v.z, 1);
@@ -68,63 +67,21 @@ public class FragmentShadedVP extends VertexProcessor {
 		// compute color at vertex:
 		Color3f ct = new Color3f(c);
 		// start with c scaled by Pipeline.ambientIntensity
-		ct.scale(Pipeline.ambientIntensity);
 
-		Point3f p = new Point3f(output.v.x, output.v.y, output.v.z);
-		Vector3f norm = new Vector3f(normal.x, normal.y, normal.z);
-		// Vector from Vertex Position to Eye
-		Vector3f toEye = new Vector3f(p);
-		toEye.scale(-1);
-
-		Vector3f lV = new Vector3f();
-		Vector3f h = new Vector3f();
-
-		// for each light, add diffuse and specular term, using normal, light
-		// direction, and eye direction
-		for (int i = 0; i < Pipeline.lights.size(); i++) {
-			PointLight l = Pipeline.lights.get(i);
-
-			// specular term uses Pipeline.specularColor and assumes light color
-			// is full white
-
-			// Calculate l and h
-			// note that light positions are given in eye space, and the camera
-			// is at the origin
-			lV.sub(l.getPosition(), p);
-			lV.normalize();
-			h.add(toEye, lV);
-			h.normalize();
-
-			// Pull Light Intensity
-			Color3f intensity = l.getIntensity();
-			// Perform Math Calculations before rather than trying to iterate
-			// through the Math library 8129418239 times.
-			double a = Math.max(0, lV.dot(norm));
-			// also uses Pipeline.specularExponent
-			double b = Math.pow(Math.max(0, norm.dot(h)),
-					Pipeline.specularExponent);
-			// Lambertian Component
-			// diffuse term uses vertex color c and light color
-			ct.x += (c.x * intensity.x * a);
-			ct.y += (c.y * intensity.y * a);
-			ct.z += (c.z * intensity.z * a);
-			// Phong Component
-			ct.x += (Pipeline.specularColor.x * intensity.x * b);
-			ct.y += (Pipeline.specularColor.y * intensity.y * b);
-			ct.z += (Pipeline.specularColor.z * intensity.z * b);
-
-		}
-		// in the end, clamp the resulting color to [0, 1] and store in
-		// attributes
-		ct.clamp(0, 1);
 		output.setAttrs(nAttr());
 		output.attrs[0] = ct.x;
 		output.attrs[1] = ct.y;
 		output.attrs[2] = ct.z;
+		output.attrs[3] = normal.x;
+		output.attrs[4] = normal.y;
+		output.attrs[5] = normal.z;
 
 		// also project vertex position to screen space
 		projection.rightMultiply(output.v);
 		viewport.rightMultiply(output.v);
+		output.attrs[6] = output.v.x;
+		output.attrs[7] = output.v.y;
+		output.attrs[8] = output.v.z;
 
 	}
 
